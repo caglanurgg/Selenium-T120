6149cdb8f1ba1a9262bab48f5bf0c71770e3a43c
caglanurgg <caglanurgg@gmail.com>
2023-09-30 18:05:02 +0300

    T120

--- src/test/java/tests/day04_xPath_cssSelector/C02_XPath.java
+++ src/test/java/tests/day04_xPath_cssSelector/C02_XPath.java
@@ -11,27 +11,43 @@ public class C02_XPath {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe"); // 1.adim
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        WebDriver driver = new ChromeDriver();  // 2.adim
+        driver.manage().window().maximize(); // 3.adim tam ekran yapiyoruz
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        // 4.adim sayfanin yuklenmesi ve web elementlerinin bulunmasi icin
+        // yaklasik bir bekleme degeri
 
-        //1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        //**1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
-        Thread.sleep(2000);
-        //2- Add Element butonuna basin
+        Thread.sleep(2000); // 2 saniye beklesin
+
+        //**2- Add Element butonuna basin
         driver.findElement(By.xpath("//button[@*='addElement()']")).click();
+        /*
+        25.satir icin
+        Relative Xpath bu 3 bilesenin belirlenen sekilde birlikte kullanilmasi ile olusur.
+        Her Xpath ile unique bir sonuc elde edilemeyebilir ancak unique bir deger mutlaka
+        bulunur.
+        //tagName[@attributeIsmi=‘attributeValue']
+       "//button[@*='addElement()']"
+        tagi button herhangi bir attribute'unun degeri de addElement olan webelementi bul dedik
+        o da unique olarak buldu
+         */
         Thread.sleep(2000);
-        //3- Delete butonu’nun gorunur oldugunu test edin
+
+        //**3- Delete butonu’nun gorunur oldugunu TEST EDIN
         WebElement deleteButonu = driver.findElement(By.xpath("//button[@*='deleteElement()']"));
         if (deleteButonu.isDisplayed()){
             System.out.println("Delete butonu testi PASSED");
         }else{
             System.out.println("Delete butonu testi FAILED");
         }
-        //4- Delete tusuna basin
+        //**4- Delete tusuna basin
         deleteButonu.click();
         Thread.sleep(2000);
-        //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+
+
+        //5- “Add/Remove Elements” yazisinin gorunur oldugunu TEST EDIN
         // WebElement addRemoveYaziElementi = driver.findElement(By.tagName("h3"));
         WebElement addRemoveYaziElementi = driver.findElement(By.xpath("//h3"));
 
